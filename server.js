465767810d773fa8735eaffe2b3da361418256db
Wellington <vovoveiowell@gmail.com>
2026-03-12 20:49:15 -0300

    agora vai 2

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
+// Lista de conexões para garantir que todos vejam o mesmo estado
+let connectedPlayers = [];
+
 io.on('connection', (socket) => {
-    // Apenas repassa a ação do jogador para os outros
-    socket.on('playerAction', (data) => {
-        io.emit('remoteAction', { from: socket.id, action: data.action, payload: data.payload });
+    connectedPlayers.push(socket.id);
+    
+    // Repassa qualquer ação de um jogador para todos os outros
+    socket.on('gameAction', (data) => {
+        io.emit('remoteAction', data);
+    });
+
+    socket.on('disconnect', () => {
+        connectedPlayers = connectedPlayers.filter(id => id !== socket.id);
     });
 });
 
